431ad7703868fabd3abb92e4fcc43c9658784547
yh <2827902614@qq.com>
2023-03-23 22:47:28 +0800

    Improve the Business menu function

--- src/movies/run/SystemMoive.java
+++ src/movies/run/SystemMoive.java
@@ -11,6 +11,8 @@ public class SystemMoive {
     public static final List<User> ALL_USERS = new ArrayList<>();
     public static Map<Business,Set<Movie>> ALL_MOVIES = new HashMap<>();
     public static final Scanner SCInput = new Scanner(System.in);
+    //静态正在登录对象
+    public static User LoginUser;
     static {
         Client c = new Client();
         c.setLogin_name("yh123456");
@@ -63,15 +65,19 @@ public class SystemMoive {
         showMenu();
     }
     public static void showMenu(){
-        System.out.println("==================HomeMenu===================");
-        System.out.println("1.登录");
-        System.out.println("2.注册");
-        System.out.println("输入操作命令: \n");
-        String command = SCInput.nextLine();
-        switch (command){
-            case "1": login(); break;
-            case  "2": enroll(); break;
-            default: break;
+        while (true) {
+            System.out.println("==================HomeMenu===================");
+            System.out.println("1.登录");
+            System.out.println("2.注册");
+            System.out.println("3.退出");
+            System.out.println("输入操作命令: \n");
+            String command = SCInput.nextLine();
+            switch (command){
+                case "1": login(); break;
+                case  "2": enroll(); break;
+                case  "3": return;
+                default: break;
+            }
         }
 
     }
@@ -88,6 +94,7 @@ public class SystemMoive {
             //
             if (loginUser != null){
                 if (loginUser.getPassword().equals(loginPassword)) {
+                    LoginUser = loginUser;
                      if(loginUser instanceof Client) {
                          showClientMenu();
                      }
@@ -106,11 +113,69 @@ public class SystemMoive {
     //客户菜单
     public static void showClientMenu(){
         System.out.println("===============ClientMenu====================");
-
+        System.out.println("欢迎"+LoginUser.getReal_name()+"您的余额为"+LoginUser.getAccount_amount());
+        System.out.println("选择需要的功能");
+        System.out.println("1.展示全部影片信息");
+        System.out.println("2.根据电影名称查询电影信息");
+        System.out.println("3.根据评分筛选全部电影");
+        System.out.println("4.评分");
+        System.out.println("5.购票");
+        System.out.println("6.退出");
+        while (true){
+            System.out.println("输入需要的功能：");
+            String command = SCInput.nextLine();
+            switch (command){
+                case "1" :
+                    break;
+                case "2" :
+                    break;
+                case "3" :
+                    break;
+                case "4" :
+                    break;
+                case "5" :
+                    break;
+                case "6" :
+                    return;
+                default:
+                    System.out.println("不存在");
+                    break;
+            }
+        }
     }
     //商家菜单
     public static void showBusinessMenu(){
-
+        System.out.println("===============BusinessMenu====================");
+        Business loginUser = (Business) LoginUser;
+        System.out.println("欢迎"+LoginUser.getReal_name()+"您的商城为"+((Business) LoginUser).getShop_name());
+        System.out.println("选择需要的功能");
+        System.out.println("1.展示全部影片信息");
+        System.out.println("2.上架电影");
+        System.out.println("3.下架电影");
+        System.out.println("4.退出");
+        while (true){
+            System.out.println("输入需要的功能：");
+            String command = SCInput.nextLine();
+            switch (command){
+                case "1" :ShowBusinessMovie();
+                    break;
+                case "2" ://Shelves();
+                    break;
+                case "3" ://Takedown();
+                    break;
+                case "4" :
+                    return;
+                default:
+                    System.out.println("不存在");
+                    break;
+            }
+        }
+    }
+    public static void ShowBusinessMovie(){
+        Set<Movie> LoginBusinessMovie =  ALL_MOVIES.get(LoginUser);
+        for (Movie movie : LoginBusinessMovie) {
+            System.out.println(movie);
+        }
     }
     public static void enroll(){
         System.out.println("==================EnrollMenu===================");
